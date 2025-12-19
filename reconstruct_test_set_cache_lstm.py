cedd12e77cd18a804e49c0490cb5bf3c9841967e
Tobias Vent <venttobias@gmail.com>
2025-12-19 14:58:48 +0100

    m

--- reconstruct_test_set_cache_lstm.py
+++ reconstruct_test_set_cache_lstm.py
@@ -15,9 +15,13 @@ Sample = namedtuple("Sample", ["features", "target", "meta"])
 
 
 #check the years to reconstruct
-START_YEAR = 2018
+START_YEAR = 2009
 END_YEAR = 2018
 
+
+#for experiment 5
+# START_YEAR = 1959
+# END_YEAR = 2018
 region = "North_Atlantic"
 experiment = "experiment_1"
 #choose the test set path pattern
